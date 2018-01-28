65d4d56ded648aed02bb95c2a0e9591d3241fbcf
ChingstaDon <D0n4lddanghuynh@gmail.com>
2018-01-28 00:56:24 -0600

    Reads and searches for a specified name from a text doc, takes in a name and line

--- fieldLookUp.py
+++ fieldLookUp.py
@@ -1,11 +1,10 @@
-def fileLine(var):
-	
-	with open("fields/primarytypeofinjury.txt", "r") as file:
+def fileLine(name, var):
+	fileName = 'fields/' + name
+        mode = 'r'
+	with open(fileName, mode) as file:
 		for num, line in enumerate(file):
 			if var == num:
 				print(line)
-		
-		
 		
 	file.closed
 
