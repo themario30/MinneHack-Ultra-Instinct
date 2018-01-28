5a1eec9ce15a01d8eb93dee38de9ed693bad6d07
Mario <TheMario80@gmail.com>
2018-01-27 21:36:38 -0600

    Add it

---
+++ CSVReader.py
@@ -0,0 +1,29 @@
+import csv
+
+class Player:
+    def __init__(self):
+        self.__Registry = dict()
+    
+    def setRegistry(self, registry, value):
+        self.__Registry[registry] = value
+    
+    def getRegistry(self, registry):
+        return self.__Registry[registry]
+        
+        
+def readFile():
+    with open("csv/Hackathon dataset.csv", newline= "\n") as file:
+        reader = csv.reader(file, delimiter= ',', quotechar= '|')
+        for row in reader:
+            print(row)
+            return
+        
+    return 0
+
+ 
+    
+def main():
+    footballPlayers = readFile
+   
+
+main()
